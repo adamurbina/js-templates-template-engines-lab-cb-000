c394449dfc239b3f70d3572c16b79e46574db735
Adam Urbina <adamurbina@gmail.com>
2019-02-17 12:39:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function postComment(){
+function createPost(){
   var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   var postTemplate = _.template(document.getElementById("post-template").innerHTML);
   var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
@@ -18,7 +18,7 @@ function postComment(){
 }
 
 
-function createPost() {
+function postComment() {
   var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
 
   var commentText = document.getElementById("commentText").value;
